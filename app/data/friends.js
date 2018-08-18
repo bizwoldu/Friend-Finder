d419b2ceb24da325cb5da678921bff97d9582abb
Bisrat Tesfandrias <jsnow@Bisrats-MacBook-Pro.local>
2018-08-17 20:22:25 -0700

    v1

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+// Variable to hold the array of friends objects data
+var friends =  {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+
+  // Export the data
+module.exports = friends;
